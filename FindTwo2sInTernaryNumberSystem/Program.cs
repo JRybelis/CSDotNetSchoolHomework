984bca332c3d844d197210251befef5c731838e1
Jacob <frizjr@protonmail.com>
2021-11-24 05:11:45 +0200

    Initial

---
+++ FindTwo2sInTernaryNumberSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindTwo2sInTernaryNumberSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
