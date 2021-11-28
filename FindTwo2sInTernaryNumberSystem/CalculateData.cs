09956bb3a990541dd77884927e62d70dabe76c86
Jacob <frizjr@protonmail.com>
2021-11-28 23:32:20 +0200

    Created ConvertIntegerRangeMemberToTernary method to convert integers to ternary. Created CheckTernaryIntegerForTwo2s method to check and return only those ternary integers that have two 2s in them.

--- FindTwo2sInTernaryNumberSystem/CalculateData.cs
+++ FindTwo2sInTernaryNumberSystem/CalculateData.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FindTwo2sInTernaryNumberSystem
 {
@@ -17,6 +20,58 @@ namespace FindTwo2sInTernaryNumberSystem
             }
 
             return integerRangeMembers;
+        }
+
+        public static List<int> ConvertIntegerRangeMemberToTernary(int integerRangeMember)
+        {
+            int dividend = integerRangeMember;
+            int quotent;
+            int lastDigit = dividend % 3;
+            List<int> InverseTernaryOutput = new List<int>();
+            List<int> TernaryIntegerDigits = new List<int>();
+            do
+            {
+                InverseTernaryOutput.Add(lastDigit);
+                
+                quotent = dividend / 3;
+                lastDigit = quotent % 3;
+            } while (quotent != 0);
+
+            InverseTernaryOutput.Reverse();
+            TernaryIntegerDigits = InverseTernaryOutput.ToList();
+
+            return TernaryIntegerDigits;
+        }
+
+        public static ArrayList CheckTernaryIntegersForTwo2s(int[] integerRangeMembers)
+        {
+            List<int> TernaryIntegerDigitsToCheck = new List<int>();
+            ArrayList TernaryNumbersWithTwo2sInThem = new ArrayList();
+            string parsedTargetTernaryNumber = String.Empty;
+            
+            foreach (var integer in integerRangeMembers)
+            {
+                TernaryIntegerDigitsToCheck = ConvertIntegerRangeMemberToTernary(integer);
+                
+                for (int i = 0; i < TernaryIntegerDigitsToCheck.Count; i++)
+                {
+                    int digitsTwoCounter = 0;
+
+                    while (digitsTwoCounter < 2)
+                    {
+                        if (TernaryIntegerDigitsToCheck[i] == 2)
+                        {
+                            digitsTwoCounter++;
+                        }    
+                    }
+                    if (digitsTwoCounter >= 2)
+                    {
+                        parsedTargetTernaryNumber = string.Join("", TernaryIntegerDigitsToCheck.ToArray());
+                    }
+                }
+                TernaryNumbersWithTwo2sInThem.Add(parsedTargetTernaryNumber);
+            }
+            return TernaryNumbersWithTwo2sInThem;
         }
     }
 }
