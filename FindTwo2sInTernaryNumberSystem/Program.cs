dd89c0f664dcb63be866938f23d6827045fe7ffe
Jacob <frizjr@protonmail.com>
2021-12-05 22:05:05 +0200

    Declared the console implementation of IWriter and IReader interfaces. Instantiated a UserMessaged class object to avoid static calls.

--- FindTwo2sInTernaryNumberSystem/Program.cs
+++ FindTwo2sInTernaryNumberSystem/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using FindTwo2sInTernaryNumberSystem.Interfaces;
+using FindTwo2sInTernaryNumberSystem.Loggers;
 
 namespace FindTwo2sInTernaryNumberSystem
 {
@@ -6,9 +8,13 @@ namespace FindTwo2sInTernaryNumberSystem
     {
         static void Main(string[] args)
         {
-            UserMessages.ApplicationLaunchMessage();
+            IWriter writer = new ConsoleLogger();
+            IReader reader = new ConsoleLogger();
+            UserMessages userMessages = new UserMessages(writer, reader);
+            
+            userMessages.ApplicationLaunchMessage();
 
-            UserMessages.PrintRangeOfIntegers();
+            //UserMessages.PrintRangeOfIntegers();
             
             UserMessages.PrintResultsMessage("The range of integers, based on the numbers you supplied has the following numbers in ternary representation with two 2s in them:");
 
