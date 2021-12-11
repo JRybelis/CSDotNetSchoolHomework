4a134899c6df316bd2c3809996ee1957a6b48285
Jacob <frizjr@protonmail.com>
2021-12-11 15:48:48 +0200

    Initialised RequestUserInput, CalculateCheckDigit and UserMessages classes and called their methods.

--- ISBN10ControlDigitGenerator/Program.cs
+++ ISBN10ControlDigitGenerator/Program.cs
@@ -11,6 +11,20 @@ namespace ISBN10ControlDigitGenerator
             IWriter writer = new ConsoleLogger();
             IReader reader = new ConsoleLogger();
 
+            RequestUserInput requestUserInput = new(writer, reader);
+            CalculateCheckDigit calculateCheckDigit = new();
+            UserMessages userMessages = new(writer, requestUserInput);
+            
+            userMessages.ApplicationLaunchMessage("Hello. This tiny app will produce an ISBN-10 code with your help. " +
+                                                  "It will take 9 digits you submit and generate the final " +
+                                                  "10th control digit, returning you the full ISBN-10 code. Enjoy!");
+
+            var userSupplied9Digits = userMessages.RequestFirst9ISBNDigits("Please enter a 9-digit-long-number, using a comma (no spaces) to separate each digit:");
+            var checkDigit = calculateCheckDigit.FindTheISBNControlDigit(userSupplied9Digits);
+            
+            
+            userMessages.PrintTheFullISBN10Out(userSupplied9Digits, checkDigit, "The ISBN-10 code is: ");
+            
             reader.Read();
         }
     }
